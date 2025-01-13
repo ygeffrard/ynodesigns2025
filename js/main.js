083306e809fd4cbe17d2a56ed60871468455041f
Yarley Geffrard <yarley@ynodesigns.com>
2025-01-12 23:04:12 -0500

    updated nav css and with blur

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function handleNavbarClass() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 30) {
+        navbar.classList.add('bg-header-gradient'); // Add class if scroll position > 30px
+    } else {
+        navbar.classList.remove('bg-header-gradient'); // Remove class if scroll position <= 30px
+    }
+}
+
+// Run on page load
+document.addEventListener('DOMContentLoaded', handleNavbarClass);
+
+// Run on scroll
+document.addEventListener('scroll', handleNavbarClass);
